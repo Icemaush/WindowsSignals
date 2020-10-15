ae04413b2a1e3d59009cb2c4d68ef5afa06288c5
Reece Pieri <46065144+Icemaush@users.noreply.github.com>
2020-10-15 23:25:08 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WindowsSignals
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textMessages = new System.Windows.Forms.TextBox();
+            this.btnStartScan = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textMessages
+            // 
+            this.textMessages.Location = new System.Drawing.Point(13, 13);
+            this.textMessages.Multiline = true;
+            this.textMessages.Name = "textMessages";
+            this.textMessages.ReadOnly = true;
+            this.textMessages.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textMessages.Size = new System.Drawing.Size(245, 215);
+            this.textMessages.TabIndex = 0;
+            this.textMessages.TabStop = false;
+            // 
+            // btnStartScan
+            // 
+            this.btnStartScan.Location = new System.Drawing.Point(264, 13);
+            this.btnStartScan.Name = "btnStartScan";
+            this.btnStartScan.Size = new System.Drawing.Size(75, 23);
+            this.btnStartScan.TabIndex = 1;
+            this.btnStartScan.TabStop = false;
+            this.btnStartScan.Text = "Start Scan";
+            this.btnStartScan.UseVisualStyleBackColor = true;
+            this.btnStartScan.Click += new System.EventHandler(this.btnStartScan_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(265, 204);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.TabStop = false;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 240);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnStartScan);
+            this.Controls.Add(this.textMessages);
+            this.Name = "Form1";
+            this.Text = "Windows Signals";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textMessages;
+        private System.Windows.Forms.Button btnStartScan;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsSignals
+{
+    public partial class Form1 : Form
+    {
+        private BackgroundWorker bw;
+
+        public Form1()
+        {
+            InitializeComponent();
+            FormClosing += new FormClosingEventHandler(Form1_Closing);
+        }
+
+        // Cancels running task before closing the application
+        private void Form1_Closing(object sender, FormClosingEventArgs e)
+        {
+            bw.CancelAsync();
+        }
+
+        // Start button function to begin a scan
+        private void btnStartScan_Click(object sender, EventArgs e)
+        {
+            if (bw == null)
+            {
+                bw = new BackgroundWorker
+                {
+                    WorkerReportsProgress = true,
+                    WorkerSupportsCancellation = true
+                };
+                bw.DoWork += new DoWorkEventHandler(bw_DoWork);
+                bw.ProgressChanged += new ProgressChangedEventHandler(bw_ProgressChanged);
+                bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bw_RunWorkerCompleted);
+            }
+            
+            if (!bw.IsBusy)
+            {
+                AppendMessage("Starting scan...", true);
+                bw.RunWorkerAsync();
+            }
+        }
+
+        // Exit button function to close the program
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            bw.CancelAsync();
+            Application.Exit();
+        }
+
+        // Runs when background worker is started
+        private void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                
+                if (bw.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                } else
+                {
+                    Thread.Sleep(100);
+                    bw.ReportProgress(i);
+                }
+            }
+        }
+
+        // Runs when background worker progress is changed
+        public void bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            string[] lines = textMessages.Lines;
+
+            if (e.ProgressPercentage == 100)
+            {
+                lines[lines.Length - 1] = "Scan progress: " + e.ProgressPercentage + "%" + Environment.NewLine;
+            } else
+            {
+                lines[lines.Length - 1] = "Scan progress: " + e.ProgressPercentage + "%";
+            }
+            
+            textMessages.Lines = lines;
+        }
+
+        // Runs when background worker task is completed
+        private void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled == true)
+            {
+                AppendMessage("", true);
+                AppendMessage("Scan cancelled.", true);
+                AppendMessage("", true);
+            } else
+            {
+                AppendMessage("Scan complete!", true);
+                AppendMessage("", true);
+            }
+        }
+
+        // Appends a message to the textbox
+        private void AppendMessage(string text, bool newLine)
+        {
+            if (newLine)
+            {
+                textMessages.AppendText(text);
+                textMessages.AppendText(Environment.NewLine);
+            } else
+            {
+                textMessages.AppendText(text);
+            }
+        }
+
+        // Detects Ctrl+C keys pressed and cancels the running task
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == (Keys.Control | Keys.C))
+            {
+                bw.CancelAsync();
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+    }
+}
